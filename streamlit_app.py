9829716110013ea35af76ec1f89bab48006d7426
coolwaterld <coolwaterld@hotmail.com>
2022-02-08 16:01:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,37 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import graphviz as graphviz
+import json
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def draw_dataflow(task_seq):
+    if task_seq is not None:
+        graph = graphviz.Digraph()
+        for task_index in range(len(task_seq)):
+            task = task_seq[task_index]
+            origin = task["origin"]
+            if len(origin)>0:
+                if isinstance (origin,list):
+                    for origin_index in range(len(origin)):
+                         graph.edge(origin[origin_index], task["id"],task["name"])
+                else:
+                    graph.edge(origin, task["id"],task["name"])
+            else:
+                if "source" in task["features"].keys():
+                    graph.edge(str(task["features"]["source"]), task["id"],task["name"])
+                         
+        st.graphviz_chart(graph)
+            
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.header("Use Case to Data Flow")
+usecase_file = st.uploaded_file = st.file_uploader("Choose a Use Case JSON file")
+col1, col2 = st.columns(2)
+if usecase_file is not None:
+    usecase_str = usecase_file.read().decode("utf-8")
+    with col1:
+        usecase_dict = json.loads(usecase_str)
+        #todo: json schema validate
+        task_sequence = usecase_dict["usecase"]["task_sequence"]
+        st.json(task_sequence)
+        
+    with col2:    
+        draw_dataflow(task_sequence)
