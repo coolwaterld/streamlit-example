3dc26052352f67e3b732a56ea38ddb436d786c25
lidong <lidong-ld@siemens.com>
2022-03-09 15:59:25 +0800

    add target node

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,17 @@
+from re import L
 import streamlit as st
 import graphviz as graphviz
 import json
 import test_schema
+import os
 
+def get_example_usecases():
+    file_list=[]
+    for root,dirs,files in os.walk(r"./input"):
+        file_list.extend(files)
+    return file_list
 
+print(get_example_usecases())
 def draw_dataflow(task_seq):
     if task_seq is not None:
         graph = graphviz.Digraph()
@@ -16,19 +24,30 @@ def draw_dataflow(task_seq):
                          graph.edge(origin[origin_index], task["id"],task["name"])
                 else:
                     graph.edge(origin, task["id"],task["name"])
-            else:
-                if "source" in task["features"].keys():
-                    graph.node(str(task["features"]["source"]),shape='box')
-                    graph.edge(str(task["features"]["source"]), task["id"],task["name"])
+            if "features" in task.keys() and "source" in task["features"].keys():
+                graph.node(str(task["features"]["source"]),shape='box')
+                graph.edge(str(task["features"]["source"]), task["id"],task["name"])
+            if "features" in task.keys() and "target" in task["features"].keys():
+                graph.node(str(task["features"]["target"]),shape='box')
+                graph.edge(task["id"],str(task["features"]["target"]))
                          
         st.graphviz_chart(graph)
             
 
 st.header("Use Case to Data Flow")
-usecase_file = st.uploaded_file = st.file_uploader("Choose a Use Case JSON file")
-col1, col2 = st.columns(2)
+usecase_file = st.file_uploader("choose a local use case JSON file")
+file_name = st.selectbox("or use example use cases",options=get_example_usecases(),index=0)
+
+usecase_str =""
 if usecase_file is not None:
     usecase_str = usecase_file.read().decode("utf-8")
+
+if usecase_file is None and file_name is not None:
+    with open('./input/'+file_name, 'r') as f:
+        usecase_str = f.read()
+
+col1, col2 = st.columns(2)
+if usecase_str is not None:
     with col1:
         usecase_dict = json.loads(usecase_str)
         #todo: json schema validate
@@ -40,7 +59,5 @@ if usecase_file is not None:
         else:
             st.error("Given JSON data is InValid")
             st.error(msg)
-        
-        
     with col2:    
         draw_dataflow(task_sequence)
