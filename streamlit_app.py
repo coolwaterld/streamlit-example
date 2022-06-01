723b1cb016ed93c317d7d33a070965e836284c4e
coolwaterld <coolwaterld@hotmail.com>
2022-06-01 19:59:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-st.header("Configuration:")
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-     bytes_data = uploaded_file.read()
-     st.write("filename:", uploaded_file.name)
-     st.write(bytes_data)
+# st.header("Configuration:")
+# uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+# for uploaded_file in uploaded_files:
+#      bytes_data = uploaded_file.read()
+#      st.write("filename:", uploaded_file.name)
+#      st.write(bytes_data)
 
 st.header("Hardware type:")
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;} </style>', unsafe_allow_html=True)
