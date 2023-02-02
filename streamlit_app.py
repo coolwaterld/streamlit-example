2d1a789eb1ac90e8b84073acdbc5ba42a11c4e27
“lidong” <coolwaterld@hotmail.com>
2023-02-02 15:31:48 +0800

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,134 +1,100 @@
-import logging
-import math
-from typing import List
+import streamlit as st
+# st.header("Configuration:")
+# uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+# for uploaded_file in uploaded_files:
+#      bytes_data = uploaded_file.read()
+#      st.write("filename:", uploaded_file.name)
+#      st.write(bytes_data)
 
-import av
-import cv2
-import numpy as np
-from streamlit_server_state import server_state, server_state_lock
-from streamlit_webrtc import WebRtcMode, create_mix_track, webrtc_streamer
-from streamlit_webrtc.component import WebRtcStreamerContext
+st.header("Hardware type:")
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;} </style>', unsafe_allow_html=True)
+hardware = st.radio("Which hardware do you want to deploy the applications?",('IEVD', 'IPC 127E', 'IPC 227E', 'IPC 427E', 'IPC 847E'))
 
-logger = logging.getLogger(__name__)
-
-
-def mixer_callback(frames: List[av.VideoFrame]) -> av.VideoFrame:
-    buf_w = 640
-    buf_h = 480
-    buffer = np.zeros((buf_h, buf_w, 3), dtype=np.uint8)
-
-    n_inputs = len(frames)
-
-    n_cols = math.ceil(math.sqrt(n_inputs))
-    n_rows = math.ceil(n_inputs / n_cols)
-    grid_w = buf_w // n_cols
-    grid_h = buf_h // n_rows
-
-    for i in range(n_inputs):
-        frame = frames[i]
-        if frame is None:
-            continue
-
-        grid_x = (i % n_cols) * grid_w
-        grid_y = (i // n_cols) * grid_h
-
-        img = frame.to_ndarray(format="bgr24")
-        src_h, src_w = img.shape[0:2]
-
-        aspect_ratio = src_w / src_h
-
-        window_w = min(grid_w, int(grid_h * aspect_ratio))
-        window_h = min(grid_h, int(window_w / aspect_ratio))
-
-        window_offset_x = (grid_w - window_w) // 2
-        window_offset_y = (grid_h - window_h) // 2
-
-        window_x0 = grid_x + window_offset_x
-        window_y0 = grid_y + window_offset_y
-        window_x1 = window_x0 + window_w
-        window_y1 = window_y0 + window_h
-
-        buffer[window_y0:window_y1, window_x0:window_x1, :] = cv2.resize(
-            img, (window_w, window_h)
-        )
-
-    new_frame = av.VideoFrame.from_ndarray(buffer, format="bgr24")
-
-    return new_frame
+resource = {"IEVD":{"cpu":100,"memory":100,"flash":100},
+"IPC 127E":{"cpu":200,"memory":200,"flash":200},
+"IPC 227E":{"cpu":300,"memory":300,"flash":300},
+"IPC 427E":{"cpu":400,"memory":400,"flash":400},
+"IPC 847E":{"cpu":500,"memory":500,"flash":500}
+}
 
 
-def main():
-    with server_state_lock["webrtc_contexts"]:
-        if "webrtc_contexts" not in server_state:
-            server_state["webrtc_contexts"] = []
-
-    with server_state_lock["mix_track"]:
-        if "mix_track" not in server_state:
-            server_state["mix_track"] = create_mix_track(
-                kind="video", mixer_callback=mixer_callback, key="mix"
-            )
-
-    mix_track = server_state["mix_track"]
-
-    self_ctx = webrtc_streamer(
-        key="self",
-        mode=WebRtcMode.SENDRECV,
-        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-        media_stream_constraints={"video": True, "audio": False},
-        source_video_track=mix_track,
-        sendback_audio=False,
-    )
-
-    if self_ctx.input_video_track:
-        mix_track.add_input_track(self_ctx.input_video_track)
-
-    with server_state_lock["webrtc_contexts"]:
-        webrtc_contexts: List[WebRtcStreamerContext] = server_state["webrtc_contexts"]
-        self_is_playing = self_ctx.state.playing
-        if self_is_playing and self_ctx not in webrtc_contexts:
-            webrtc_contexts.append(self_ctx)
-            server_state["webrtc_contexts"] = webrtc_contexts
-        elif not self_is_playing and self_ctx in webrtc_contexts:
-            webrtc_contexts.remove(self_ctx)
-            server_state["webrtc_contexts"] = webrtc_contexts
-
-    # Audio streams are transferred in SFU manner
-    # TODO: Create MCU to mix audio streams
-    for ctx in webrtc_contexts:
-        if ctx == self_ctx or not ctx.state.playing:
-            continue
-        webrtc_streamer(
-            key=f"sound-{id(ctx)}",
-            mode=WebRtcMode.RECVONLY,
-            rtc_configuration={
-                "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-            },
-            media_stream_constraints={"video": True, "audio": False},
-            source_audio_track=ctx.input_audio_track,
-            desired_playing_state=ctx.state.playing,
-        )
+cpu_usage_value = 0
+memory_usage_value = 0
+flash_usage_value = 0
+# st.write('cpu usage:')
+# cpu_usage_percent = st.progress(cpu_usage_value/resource[hardware]["cpu"])
+# st.write('memory usage:')
+# memory_usage_percent = st.progress(memory_usage_value/resource[hardware]["memory"])
+# st.write('flash usage:')
+# flash_usage_percent = st.progress(flash_usage_value/resource[hardware]["flash"])
 
 
-if __name__ == "__main__":
-    import os
 
-    DEBUG = os.environ.get("DEBUG", "false").lower() not in ["false", "no", "0"]
+st.header("Application:")
+with st.expander("Optimized S7"):
+     Optimized_S7 = st.checkbox('Optimized S7')
+     Optimized_S7_Connections = st.selectbox(
+          'Connections?',
+          (1,8),
+          key=1,disabled=not Optimized_S7)
 
-    logging.basicConfig(
-        format="[%(asctime)s] %(levelname)7s from %(name)s in %(pathname)s:%(lineno)d: "
-        "%(message)s",
-        force=True,
-    )
+     if Optimized_S7:
+          cpu_usage_value=cpu_usage_value+Optimized_S7_Connections*5
+          memory_usage_value = memory_usage_value+Optimized_S7_Connections*5
+          flash_usage_value = flash_usage_value+Optimized_S7_Connections*5
 
-    logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
+     Optimized_S7_Aquisition_Cycle = st.selectbox(
+          'Aquisition Cycle?',
+          (100,500),
+          key=2,disabled=not Optimized_S7)
+     if Optimized_S7:
+          cpu_usage_value=cpu_usage_value+Optimized_S7/10
+          memory_usage_value = memory_usage_value+Optimized_S7/10
+          flash_usage_value = flash_usage_value+Optimized_S7/10
 
-    st_webrtc_logger = logging.getLogger("streamlit_webrtc")
-    st_webrtc_logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
+     Optimized_S7_tags = st.select_slider(
+          'Tags',
+          options=[600,800,1000,1200,1400,1600,1800,2000],
+          key=3,disabled=not Optimized_S7)
 
-    aioice_logger = logging.getLogger("aioice")
-    aioice_logger.setLevel(logging.WARNING)
+     if Optimized_S7:
+          cpu_usage_value=cpu_usage_value+Optimized_S7_tags/100
+          memory_usage_value = memory_usage_value+Optimized_S7_tags/100
+          flash_usage_value = flash_usage_value+Optimized_S7_tags/100
+with st.expander("OPC UA"):
+     OPC_UA = st.checkbox('OPC UA')
+     OPC_UA_Connections = st.selectbox(
+          'Connections?',
+          (1,8),
+          key=4,disabled=not OPC_UA)
+     if OPC_UA:
+          cpu_usage_value=cpu_usage_value+OPC_UA_Connections*6
+          memory_usage_value = memory_usage_value+OPC_UA_Connections*6
+          flash_usage_value = flash_usage_value+OPC_UA_Connections*6
 
-    fsevents_logger = logging.getLogger("fsevents")
-    fsevents_logger.setLevel(logging.WARNING)
+     OPC_UA_Aquisition_Cycle = st.selectbox(
+          'Aquisition Cycle?',
+          (80,640),
+          key=5,disabled=not OPC_UA)
+     if OPC_UA:
+          cpu_usage_value=cpu_usage_value+OPC_UA_Aquisition_Cycle/8
+          memory_usage_value = memory_usage_value+OPC_UA_Aquisition_Cycle/8
+          flash_usage_value = flash_usage_value+OPC_UA_Aquisition_Cycle/8
 
-    main()
+     OPC_UA_tags = st.select_slider(
+          'Tags',
+          options=[500,1000,1500,2000],
+          key = 6,disabled=not OPC_UA)
+     if OPC_UA:
+          cpu_usage_value=cpu_usage_value+OPC_UA_tags/50
+          memory_usage_value = memory_usage_value+OPC_UA_tags/50
+          flash_usage_value = flash_usage_value+OPC_UA_tags/50
+
+     with st.sidebar:
+          st.header("Resurce:")
+
+          st.metric("CPU", cpu_usage_value/resource[hardware]["cpu"])
+          st.metric("Memory", memory_usage_value/resource[hardware]["memory"])
+          st.metric("Flash", flash_usage_value/resource[hardware]["flash"])
+          if cpu_usage_value/resource[hardware]["cpu"] > 1 or  cpu_usage_value/resource[hardware]["memory"]> 1 or flash_usage_value/resource[hardware]["flash"]>1:
+               st.warning("Hardware resource overload! Suggest change a powerful machine!")
