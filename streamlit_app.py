00bf0150331b27fb01fe9bde4e44bbf0007a29a9
Dong Li <lidong-ld@siemens.com>
2024-02-26 14:38:33 +0800

    panels filter and validation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+
 st.set_page_config(layout="wide")
 
-pd_list_result = []
 uploaded_file = st.file_uploader("选择需要转换的配置文件")
 
 
@@ -22,6 +21,8 @@ def merge_sheets(filename):
     dfs = [xls.parse(index)for index in filtered_items]
     return pd.concat(dfs,ignore_index = True)
 
+pd_list_result = [] # used for get result from data_editor to generate final csv
+# used for keep all the manual changes in modbus
 if "controller_de_changed" not in st.session_state:
     st.session_state["controller_de_changed"] = {}
 if "p2_de_changed" not in st.session_state:
@@ -29,21 +30,23 @@ if "p2_de_changed" not in st.session_state:
 if "linkage_de_changed" not in st.session_state:
     st.session_state["linkage_de_changed"] = {}
 
+# update 
 def onChange(key):
     st.session_state[key+"_changed"].update(st.session_state[key]["edited_rows"])
     
-def validateUnique(serie,value,index):
-
-    indexlist = serie[serie == value].index.tolist()
-    if index is not None:
-        indexlist.remove(index)
-    return indexlist
 
 increment = 1
+controller_result = pd.DataFrame()
+p2_result = pd.DataFrame()
+linkage_result = pd.DataFrame()
 if uploaded_file is not None:
     options = st.multiselect('选择需要转换的类型',['控制器节点', 'P2设备', '联动盘']) 
 ##############################################
     df_controler = load_sheet(uploaded_file.name, '控制器')
+    panels = st.multiselect("选择需要转换的panel",df_controler["名称"])
+    df_controler = df_controler[df_controler['名称'].isin(panels)] # filter: only keep rows whose panel in [panels]
+    panels_tuples = df_controler.apply(lambda row: (row['系统地址'], row['控制器地址']), axis=1)
+
     df_controler = df_controler.iloc[:, [0, 1, 2, 3, 4, 5, 6]]
     df_controler.rename(columns={'IP地址': '回路地址', '子网掩码': '点地址', '默认网关': '通道地址'}, inplace=True)
     df_controler = df_controler.reset_index(drop=True) # make sure index is unique
@@ -56,9 +59,15 @@ if uploaded_file is not None:
             df_controler.loc[key, "Modbus"] = st.session_state["controller_de_changed"][key]["Modbus"]
         controller_result = st.data_editor(df_controler, use_container_width=True, key="controller_de", disabled=[0, 1, 2, 3, 4, 5, 6],on_change=onChange,args=["controller_de"])
         pd_list_result.append(controller_result)
+    else:
+        controller_result = pd.DataFrame()
+        st.session_state["controller_de_changed"] = {}
+
 # ##############################################
     df_loop = load_sheet(uploaded_file.name, '回路')
     df_detect_loop = df_loop[df_loop['类型'].str.contains('探测总线')]
+    df_detect_loop = df_detect_loop[df_detect_loop[['系统地址', '控制器地址']].apply(tuple, axis=1).isin(panels_tuples)]# filter: only keep rows whose panel in [panels]
+
     result_tuples = df_detect_loop.apply(lambda row: (row['系统地址'], row['控制器地址'], row['回路地址']), axis=1)
     mergedf = merge_sheets(uploaded_file.name)
     df_p2 = mergedf[mergedf[['系统地址', '控制器地址', '回路地址']].apply(tuple, axis=1).isin(result_tuples)]
@@ -73,8 +82,12 @@ if uploaded_file is not None:
             df_p2.loc[key, "Modbus"] = st.session_state["p2_de_changed"][key]["Modbus"]
         p2_result = st.data_editor(df_p2, use_container_width=True,key="p2_de",disabled=[0, 1, 2, 3, 4, 5, 6],on_change=onChange,args=["p2_de"])
         pd_list_result.append(p2_result)
+    else:
+        p2_result = pd.DataFrame()
+        st.session_state["p2_de_changed"] = {}
 ##############################################
     df_linkage = mergedf[(mergedf["回路地址"] == 33) & (mergedf['类型'].str.contains('联动盘'))]
+    df_linkage = df_linkage[df_linkage[['系统地址', '控制器地址']].apply(tuple, axis=1).isin(panels_tuples)] # filter: only keep rows whose panel in [panels]
     df_linkage = df_linkage.reset_index(drop=True) # make sure index is unique
 
     df_linkage = df_linkage.iloc[:, [0, 1, 2, 3, 4, 5, 6,]]
@@ -88,26 +101,43 @@ if uploaded_file is not None:
         linkage_result = st.data_editor(df_linkage, use_container_width=True,key="linkage_de",disabled=[0, 1, 2, 3, 4, 5, 6],on_change=onChange,args=["linkage_de"])
         st.session_state["linkage_de_changed"].update(st.session_state.linkage_de["edited_rows"])
         pd_list_result.append(linkage_result)
+    else:
+        linkage_result = pd.DataFrame()
+        st.session_state["linkage_de_changed"] = {}
 ##############################################
+    def validateUnique(serie,value,index):
+        indexlist = serie[serie == value].index.tolist()
+        if index is not None:
+            indexlist.remove(index)
+        return indexlist
+    def validateAll(key,value,whichResult):
+        if not controller_result.empty:
+            ret = validateUnique(controller_result["Modbus"],val,key if whichResult == 'controller_result'else None)
+            if len(ret)>0:
+                # st.write(whichResult+"["+str(key)+"],conflict with controller_result" +str(ret))
+                st.toast(whichResult+"["+str(key)+"],conflict with controller_result" +str(ret),icon="⚠️")
+        if not p2_result.empty:
+            ret = validateUnique(p2_result["Modbus"],val,key if whichResult == 'p2_result'else None)
+            if len(ret)>0:
+                st.toast(whichResult+"["+str(key)+"],conflict with p2_result" +str(ret),icon="⚠️")
+        if not linkage_result.empty:
+            ret = validateUnique(linkage_result["Modbus"],val,key if whichResult == 'linkage_result'else None)
+            if len(ret)>0:
+                st.toast(whichResult+"["+str(key)+"],conflict with linkage_result" +str(ret),icon="⚠️")
+
     if len(pd_list_result) > 0:
         appended_df = pd.concat(pd_list_result, ignore_index=True)
-        with st.sidebar:
-            # st.write(appended_df)
-            st.write(st.session_state["controller_de_changed"])
-            # st.write(st.session_state["p2_de_changed"])
-            # st.write(st.session_state["linkage_de_changed"])
-            for key in st.session_state["controller_de_changed"]:
-                val = st.session_state["controller_de_changed"][key]["Modbus"]
-                ret = validateUnique(controller_result["Modbus"],val,key)
-                if len(ret)>0:
-                    st.write("controller_result["+str(key)+"],conflict with controller_result" +str(ret))
-                ret = validateUnique(p2_result["Modbus"],val,None)
-                if len(ret)>0:
-                    st.write("controller_result["+str(key)+"],conflict with p2_result" +str(ret))
-                ret = validateUnique(linkage_result["Modbus"],val,None)
-                if len(ret)>0:
-                    st.write("controller_result["+str(key)+"],conflict with linkage_result" +str(ret))
-                
+
+        for key in st.session_state["controller_de_changed"]:
+            val = st.session_state["controller_de_changed"][key]["Modbus"]
+            validateAll(key,val,'controller_result')
+        for key in st.session_state["p2_de_changed"]:
+            val = st.session_state["p2_de_changed"][key]["Modbus"]
+            validateAll(key,val,'p2_result')
+        for key in st.session_state["linkage_de_changed"]:
+            val = st.session_state["linkage_de_changed"][key]["Modbus"]
+            validateAll(key,val,'linkage_result')
+        
         csv_result = appended_df.to_csv().encode('utf-8')
         st.download_button(
             label="Download data as CSV",
@@ -115,3 +145,4 @@ if uploaded_file is not None:
             file_name=uploaded_file.name.split(".")[0]+'.csv',
             mime='text/csv',
         )
+
